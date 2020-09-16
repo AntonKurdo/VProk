98d31f4e0e84d66323a610eca154c4a914a97700
Anton Kurdo <anton.kurdo@gmail.com>
2020-09-16 15:28:35 +0300

    Updates

---
+++ precache-manifest.a9d57fb4e56950309d1499a81358b86e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece59038c24c2c0e44e3c63ccc1ae935",
+    "url": "/VProk/index.html"
+  },
+  {
+    "revision": "37f8bcc3deae321d4242",
+    "url": "/VProk/static/js/2.9fb4b4ba.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/VProk/static/js/2.9fb4b4ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a364f005ecfd6030070",
+    "url": "/VProk/static/js/main.e863a348.chunk.js"
+  },
+  {
+    "revision": "28825e220e904a33108e",
+    "url": "/VProk/static/js/runtime-main.5c976bd2.js"
+  },
+  {
+    "revision": "809b46220588f6f0d858e48e228fc28f",
+    "url": "/VProk/static/media/1.809b4622.jpg"
+  },
+  {
+    "revision": "716665be600f6896aa65e8dae9279a8f",
+    "url": "/VProk/static/media/2.716665be.jpg"
+  },
+  {
+    "revision": "42e60c79e6fb6d4a120312f9a85c6ece",
+    "url": "/VProk/static/media/3.42e60c79.jpg"
+  },
+  {
+    "revision": "5c679a28f4afbde364adba3e6c167f7f",
+    "url": "/VProk/static/media/4.5c679a28.jpg"
+  },
+  {
+    "revision": "3918a8445d942a1f5db3597476a73674",
+    "url": "/VProk/static/media/5.3918a844.jpg"
+  },
+  {
+    "revision": "6dbc4f94099494bcbd158c3d7d3ddbd9",
+    "url": "/VProk/static/media/6.6dbc4f94.jpg"
+  },
+  {
+    "revision": "a9dad2b7c50c1f732c64ec59a1c51555",
+    "url": "/VProk/static/media/contacts_bg.a9dad2b7.jpg"
+  },
+  {
+    "revision": "cd332c134a87b203f88c7a99d8b5a2b9",
+    "url": "/VProk/static/media/header_bg.cd332c13.jpg"
+  }
+]);
